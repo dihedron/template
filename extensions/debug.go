afc5f0098c2ad106d6835e169346d69985544374
Andrea Funtò <dihedron.dev@gmail.com>
2026-03-25 22:00:20 +0100

    Fix potential string escaspe vulnerability

--- extensions/debug.go
+++ extensions/debug.go
@@ -16,7 +16,7 @@ func DumpArgs(args ...any) (string, error) {
 	var result strings.Builder
 	if args != nil {
 		for i, arg := range args {
-			result.WriteString(fmt.Sprintf("%d => '%s' (%T)\n", i, toJSON(arg), arg))
+			result.WriteString(fmt.Sprintf("%d => %s (%T)\n", i, toJSON(arg), arg))
 		}
 		fmt.Fprintln(os.Stderr, result.String())
 		return result.String(), nil
