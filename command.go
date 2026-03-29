707693bb78007d9130616deae8bf0999fa3ffc16
Andrea Funtò <dihedron.dev@gmail.com>
2026-03-29 21:31:41 +0200

    Start adding support fr remote templates

--- command.go
+++ command.go
@@ -1,30 +1,41 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"io"
 	"maps"
+	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/dihedron/rawdata"
 	"github.com/dihedron/template/extensions"
+	"github.com/jlaffaye/ftp"
 	"golang.org/x/exp/slog"
 )
 
+// Command is the main command for the application.
 type Command struct {
 	Input     *Input   `short:"i" long:"input" description:"The input data, either as an inline JSON value or as a @file (in JSON or YAML format)." optional:"yes" env:"TEMPLATE_INPUT"`
 	Templates []string `short:"t" long:"template" description:"The paths of all the templates and subtemplates on disk; the main template must be the first." required:"yes"`
 	Output    string   `short:"o" long:"output" description:"The path to the output file." optional:"yes" env:"TEMPLATE_OUTPUT"`
 }
 
+// Input is the input data for the template.
 type Input struct {
 	Data any
 }
 
+// UnmarshalFlag unmarshals the input data from a string;
+// if the string starts with '@', it is treated as a file path,
+// otherwise it is treated as an inline JSON value.
 func (i *Input) UnmarshalFlag(value string) error {
 	var err error
 	i.Data, err = rawdata.Unmarshal(value)
@@ -34,6 +45,7 @@ func (i *Input) UnmarshalFlag(value string) error {
 	return err
 }
 
+// Execute executes the command.
 func (cmd *Command) Execute(args []string) error {
 	var err error
 
@@ -67,11 +79,34 @@ func (cmd *Command) Execute(args []string) error {
 	} else {
 		output = os.Stdout
 	}
+	// ensure the output is closed
+	if output, ok := output.(io.WriteCloser); ok {
+		defer output.Close()
+	}
 
 	// populate the functions map
 	functions := template.FuncMap{}
 	maps.Copy(functions, extensions.FuncMap())
 	maps.Copy(functions, sprig.FuncMap())
+
+	// // download the templates if needed
+	// for _, t := range cmd.Templates {
+	// 	if strings.HasPrefix(t, "http://") || strings.HasPrefix(t, "https://") {
+	// 		data, err := downloadHTTP(t)
+	// 		if err != nil {
+	// 			slog.Error("cannot download template", "error", err)
+	// 			return fmt.Errorf("error downloading template %s: %w", t, err)
+	// 		}
+	// 		cmd.Templates = append(cmd.Templates, string(data))
+	// 	} else if strings.HasPrefix(t, "ftp://") || strings.HasPrefix(t, "ftps://") || strings.HasPrefix(t, "sftp://") {
+	// 		data, err := downloadFTP(t)
+	// 		if err != nil {
+	// 			slog.Error("cannot download template", "error", err)
+	// 			return fmt.Errorf("error downloading template %s: %w", t, err)
+	// 		}
+	// 		cmd.Templates = append(cmd.Templates, string(data))
+	// 	}
+	// }
 
 	// parse the templates
 	main := path.Base(cmd.Templates[0])
@@ -87,4 +122,136 @@ func (cmd *Command) Execute(args []string) error {
 		return fmt.Errorf("error applying data to template: %w", err)
 	}
 	return nil
+}
+
+func read(path string) ([]byte, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+	return data, nil
+}
+
+// downloadHTTP downloads a file from an HTTP or HTTPS server.
+// The URL can be in the format:
+//
+//	http://[<user>[:<password>]@]<host>[:<port>][<path>]
+//	https://[<user>[:<password>]@]<host>[:<port>][<path>]
+//
+// where:
+//
+// - <user> is the username
+// - <password> is the password
+// - <host> is the hostname
+// - <port> is the port number
+// - <path> is the path to the file
+func downloadHTTP(rawURL string) ([]byte, error) {
+	// send the GET request
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make GET request: %w", err)
+	}
+
+	// ensure the body is closed to prevent memory leaks
+	defer resp.Body.Close()
+
+	// check for a successful status code
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// read the entire body into a []byte
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return data, nil
+}
+
+// downloadFTP downloads a file from an FTP or SFTP server.
+// The URL can be in the format:
+//
+//	ftp://[<user>[:<password>]@]<host>[:<port>][<path>]
+//	ftps://[<user>[:<password>]@]<host>[:<port>][<path>]
+//	sftp://[<user>[:<password>]@]<host>[:<port>][<path>]
+//
+// where:
+//
+// - <user> is the username
+// - <password> is the password
+// - <host> is the hostname
+// - <port> is the port number
+// - <path> is the path to the file
+func downloadFTP(rawURL string) ([]byte, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid URL: %w", err)
+	}
+
+	scheme := strings.ToLower(u.Scheme)
+
+	user := ""
+	pass := ""
+	if u.User != nil {
+		if u.User.Username() != "" && user == "" {
+			user = u.User.Username()
+		}
+		if p, ok := u.User.Password(); ok && pass == "" {
+			pass = p
+		}
+	}
+
+	var useTLS bool
+	switch scheme {
+	case "ftp":
+		useTLS = false
+	case "ftps":
+		useTLS = true
+	default:
+		return nil, fmt.Errorf("unsupported protocol for FTP download: %s", scheme)
+	}
+
+	host := u.Host
+	path := u.Path
+
+	if !strings.Contains(host, ":") {
+		if useTLS {
+			host += ":990"
+		} else {
+			host += ":21"
+		}
+	}
+
+	c, err := ftp.Dial(host, ftp.DialWithTimeout(10*time.Second))
+	if err != nil {
+		return nil, fmt.Errorf("ftp dial: %w", err)
+	}
+	defer c.Quit()
+
+	if useTLS {
+		err = c.AuthTLS(&tls.Config{InsecureSkipVerify: true})
+		if err != nil {
+			return nil, fmt.Errorf("ftp auth tls: %w", err)
+		}
+	}
+
+	// Login with anonymous as default if user is empty
+	if user == "" {
+		user = "anonymous"
+		pass = "anonymous"
+	}
+
+	err = c.Login(user, pass)
+	if err != nil {
+		return nil, fmt.Errorf("ftp login: %w", err)
+	}
+
+	r, err := c.Retr(path)
+	if err != nil {
+		return nil, fmt.Errorf("ftp retrieve: %w", err)
+	}
+	defer r.Close()
+
+	return io.ReadAll(r)
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dihedron/rawdata v1.0.3
 	github.com/jedib0t/go-pretty/v6 v6.7.8
 	github.com/jessevdk/go-flags v1.6.1
+	github.com/jlaffaye/ftp v0.2.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/exp v0.0.0-20260312153236-7ab1446f8b90
 )
@@ -18,6 +19,8 @@ require (
 	github.com/Masterminds/semver/v3 v3.4.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/mattn/go-runewidth v0.0.21 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
